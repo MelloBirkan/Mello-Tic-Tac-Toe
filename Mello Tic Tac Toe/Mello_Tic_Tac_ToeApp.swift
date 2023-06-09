0c6a88a2a42e23c58c1e7eca972c929af61f045b
marcellogonzattobirkan <mgbirkan@icloud.com>
2023-06-09 17:56:46 -0300

    Initial Commit

---
+++ Mello Tic Tac Toe/Mello_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mello_Tic_Tac_ToeApp.swift
+//  Mello Tic Tac Toe
+//
+//  Created by Marcello Gonzatto Birkan on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Mello_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
